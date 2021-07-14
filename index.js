22abba4c18982f760df0762e4c17cc7a4292f944
Daniel Haanpaa <dhaanpaa@djhent.us>
2021-07-13 18:36:47 -0700

    Faker: Test

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var faker = require('faker');
+require('dotenv').config();
+
+const faker = require('faker');
 
 
 for (let index = 0; index < 256; index++) {
-  var randomCard = faker.helpers.createCard(); // random contact card containing many properties
-  console.log(randomCard)
-  
+  const testCard = {
+    name: faker.name.firstName('female') + ", " + faker.name.lastName('female')
+  };
+
+  console.log(testCard)
+
 }
 
 
